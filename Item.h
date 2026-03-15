b82b95fd72459ef949eed7843756c555c7dd9977
Maksym-github <maksymdark11@gmail.com>
2026-03-15 16:06:02 +0200

    Add files via upload

---
+++ Item.h
@@ -0,0 +1,21 @@
+#ifndef ITEM_H_INCLUDED
+#define ITEM_H_INCLUDED
+
+struct Item{
+    int id;
+    std::string name;
+    int damage;
+};
+std::vector<Item> ItemDB={
+    {1, "Sword1", 5},
+    {2, "Sword2", 10},
+    {3, "Sword3", 20}
+};
+Item getItem(int id){
+    for(int i=0;i<ItemDB.size();i++){
+        if(ItemDB[i].id == id)
+            return ItemDB[i];
+    }
+}
+
+#endif // ITEM_H_INCLUDED
