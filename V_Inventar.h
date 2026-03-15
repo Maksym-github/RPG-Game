ec3f4bfb14087de6370f1cd1c5cff2691f6a5c99
Maksym-github <maksymdark11@gmail.com>
2026-03-15 09:44:48 +0200

    Update V_Inventar.h

--- V_Inventar.h
+++ V_Inventar.h
@@ -1,20 +1,44 @@
 #ifndef V_INVENTAR_H_INCLUDED
 #define V_INVENTAR_H_INCLUDED
 
+vector<int> inv;
+int sizex=15, sizey=8;
+void items(int x, int y, Item & item){
+    switch(item.id){
+    case 1:{
+        setcurspos(x, y);
+        cout<<"   ||"; curspos.Y++; SetConsoleCursorPosition(CursOut, curspos);
+        cout<<"   /\\"; curspos.Y++; SetConsoleCursorPosition(CursOut, curspos);
+        cout<<"  |  |"; curspos.Y++; SetConsoleCursorPosition(CursOut, curspos);
+        cout<<"  /  \\ "; curspos.Y++; SetConsoleCursorPosition(CursOut, curspos);
+        cout<<"--------"; curspos.Y++; SetConsoleCursorPosition(CursOut, curspos);
+        cout<<" |    |"; curspos.Y++; SetConsoleCursorPosition(CursOut, curspos);
+        cout<<" |    |"; curspos.Y++; SetConsoleCursorPosition(CursOut, curspos);
+        cout<<" |----|"; curspos.Y++; SetConsoleCursorPosition(CursOut, curspos);
+        break;
+        }
+    }
+}
 void inventar(Player & player, int x, int y){
     setcurspos(x, y);
+    SetConsoleOutputCP(866);
     for(int c=0; c<5; c++){
-        for(int j=0; j<10; j++){
+        for(int j=0; j<sizex; j++){
             cout<<'#';
         }
-        for(int i=0; i<6; i++){
-            curspos.X=x+10; SetConsoleCursorPosition(CursOut, curspos);
+        if(inv.empty()==0){
+            //setcurspos(x+1, y+5); cout<<player.GetNameItem();
+            items(x+1, y+1, player.GetItemId());
+            inv.at(c)=player.GetItemId();
+        }
+        for(int i=0; i<sizey; i++){
+            curspos.X=x+sizex; SetConsoleCursorPosition(CursOut, curspos);
             cout<<'#';
             curspos.X=x; SetConsoleCursorPosition(CursOut, curspos);
             cout<<'#';
             curspos.Y++; SetConsoleCursorPosition(CursOut, curspos);
         }
-        for(int j=0; j<=10; j++){
+        for(int j=0; j<=sizex; j++){
             cout<<'#';
         }
         curspos.Y++; curspos.Y--; SetConsoleCursorPosition(CursOut, curspos);
