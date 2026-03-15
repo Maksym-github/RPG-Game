d2d73d6d47c581a56337a0ed3ee7638bfda836d7
Maksym-github <maksymdark11@gmail.com>
2026-03-15 16:02:58 +0200

    Update V_Inventar.h

--- V_Inventar.h
+++ V_Inventar.h
@@ -1,14 +1,27 @@
 #ifndef V_INVENTAR_H_INCLUDED
 #define V_INVENTAR_H_INCLUDED
 
+int sizex=16, sizey=8;
 vector<int> inv;
-int sizex=15, sizey=8;
-void items(int x, int y, Item & item){
-    switch(item.id){
+void items(int x, int y, int c){
+    switch(c){
     case 1:{
+        setcurspos(x, y);
+        cout<<"  /\\"; curspos.Y++; SetConsoleCursorPosition(CursOut, curspos);
+        cout<<" |  |"; curspos.Y++; SetConsoleCursorPosition(CursOut, curspos);
+        cout<<" |  |"; curspos.Y++; SetConsoleCursorPosition(CursOut, curspos);
+        cout<<" /  \\ "; curspos.Y++; SetConsoleCursorPosition(CursOut, curspos);
+        cout<<"------"; curspos.Y++; SetConsoleCursorPosition(CursOut, curspos);
+        cout<<" |  |"; curspos.Y++; SetConsoleCursorPosition(CursOut, curspos);
+        cout<<" |  |"; curspos.Y++; SetConsoleCursorPosition(CursOut, curspos);
+        cout<<" |--|"; curspos.Y++; SetConsoleCursorPosition(CursOut, curspos);
+        break;
+        }
+    case 2:{
         setcurspos(x, y);
         cout<<"   ||"; curspos.Y++; SetConsoleCursorPosition(CursOut, curspos);
         cout<<"   /\\"; curspos.Y++; SetConsoleCursorPosition(CursOut, curspos);
+        cout<<"  |  |"; curspos.Y++; SetConsoleCursorPosition(CursOut, curspos);
         cout<<"  |  |"; curspos.Y++; SetConsoleCursorPosition(CursOut, curspos);
         cout<<"  /  \\ "; curspos.Y++; SetConsoleCursorPosition(CursOut, curspos);
         cout<<"--------"; curspos.Y++; SetConsoleCursorPosition(CursOut, curspos);
@@ -26,11 +39,6 @@ void inventar(Player & player, int x, int y){
         for(int j=0; j<sizex; j++){
             cout<<'#';
         }
-        if(inv.empty()==0){
-            //setcurspos(x+1, y+5); cout<<player.GetNameItem();
-            items(x+1, y+1, player.GetItemId());
-            inv.at(c)=player.GetItemId();
-        }
         for(int i=0; i<sizey; i++){
             curspos.X=x+sizex; SetConsoleCursorPosition(CursOut, curspos);
             cout<<'#';
@@ -43,6 +51,9 @@ void inventar(Player & player, int x, int y){
         }
         curspos.Y++; curspos.Y--; SetConsoleCursorPosition(CursOut, curspos);
     }
+    items(x+1, y+1, 1);
+    items(x+1, y+10, 2);
+    items(x+1, y+20, 3);
 }
 
 #endif // V_INVENTAR_H_INCLUDED
